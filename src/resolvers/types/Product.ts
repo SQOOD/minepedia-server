b8e48202a7e2563399eeba8f7de9eceab2350b74
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2020-10-25 11:09:06 +0700

    fix

--- src/resolvers/types/Product.ts
+++ src/resolvers/types/Product.ts
@@ -1,31 +1,33 @@
-import { objectType, extendType } from '@nexus/schema';
+import { objectType, extendType } from '@nexus/schema'
 
 export const Product = objectType({
   name: 'Product',
   definition(t) {
-    t.model.id();
-    t.model.productId();
-    t.model.name();
-    t.model.profile();
-    t.model.profileId();
-    t.model.unit();
-    t.model.tkdn();
-    t.model.certificate();
-    t.model.category();
-    t.model.minPurchase();
-    t.model.description();
-    t.model.createdAt();
-    t.model.priceRange();
-    t.model.customizable();
-    t.model.details();
+    t.model.id()
+    t.model.productId()
+    t.model.name()
+    t.model.profile()
+    t.model.profileId()
+    t.model.unit()
+    t.model.tkdn()
+    t.model.hscode()
+    t.model.country()
+    t.model.certificate()
+    t.model.category()
+    t.model.minPurchase()
+    t.model.description()
+    t.model.createdAt()
+    t.model.priceRange()
+    t.model.customizable()
+    t.model.details()
   },
-});
+})
 
 export const productQuery = extendType({
   type: 'Query',
   definition(t) {
-    t.crud.product();
-    t.crud.products({ filtering: true, ordering: true });
+    t.crud.product()
+    t.crud.products({ filtering: true, ordering: true })
 
     t.field('productsCount', {
       type: 'Int',
@@ -33,21 +35,21 @@ export const productQuery = extendType({
         where: 'ProductWhereInput',
       },
       async resolve(_root, args, ctx) {
-        return ctx.prisma.product.count(args);
+        return ctx.prisma.product.count(args)
       },
-    });
+    })
   },
-});
+})
 
 export const productMutation = extendType({
   type: 'Mutation',
   definition(t) {
-    t.crud.createOneProduct();
-    t.crud.updateOneProduct();
-    t.crud.upsertOneProduct();
-    t.crud.deleteOneProduct();
+    t.crud.createOneProduct()
+    t.crud.updateOneProduct()
+    t.crud.upsertOneProduct()
+    t.crud.deleteOneProduct()
 
-    t.crud.updateManyProduct();
-    t.crud.deleteManyProduct();
+    t.crud.updateManyProduct()
+    t.crud.deleteManyProduct()
   },
-});
+})
